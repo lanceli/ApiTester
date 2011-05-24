b6d4adc6d48e06466e596e19dc2ad6c1e8b4c72c
Kai Wu <kaiwu2004@gmail.com>
2011-05-24 15:41:49 +0800

    Initial Commit

---
+++ ApiTester/ApiTesterAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  ApiTesterAppDelegate.h
+//  ApiTester
+//
+//  Created by WU Kai on 5/24/11.
+//  Copyright 2011 None. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ApiTesterAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
