4d887c2b84ec4584cb2dbdec85a5951815565c6a
Kai Wu <kaiwu2004@gmail.com>
2011-05-26 12:47:11 +0800

    refactor according to oauthcomsumer

--- ApiTester/Classes/Domain/ATProvider.m
+++ ApiTester/Classes/Domain/ATProvider.m
@@ -35,6 +35,33 @@
     [super dealloc];
 }
 
+/**
+ *
+        _title = @"Github v2";
+        _description = [NSString stringWithFormat:@"Total %d APIs,develop.github.com",[_apis count]];
+        _appKey = @"ef4a0ff8a0d391632ccf";
+        _appSecret = @"6df83097b04f791cf2261db25827b657ed5fc199";
+        _oauthRequestURL = [NSURL URLWithString:@"https://github.com/login/oauth/request_token?"];
+        _oauthAccessURL = [NSURL URLWithString:@"https://github.com/login/oauth/access_token?"];
+        _oauthAuthorizeURL = [NSURL URLWithString:@"https://github.com/login/oauth/authorize?"];
+ *
+        _title = @"腾讯微博";
+        _description = [NSString stringWithFormat:@"Total %d APIs,open.t.qq.com",[_apis count]];
+        _appKey = @"9bacf000008941dca9dd50b5e9e8ae06";
+        _appSecret = @"a3afac95fa82822bef92bbb4b868cfde";
+        _oauthRequestURL = [NSURL URLWithString:@"https://open.t.qq.com/cgi-bin/request_token"];
+        _oauthAccessURL = [NSURL URLWithString:@"https://open.t.qq.com/cgi-bin/access_token"];
+        _oauthAuthorizeURL = [NSURL URLWithString:@"https://open.t.qq.com/cgi-bin/authorize"];
+ *
+        _title = @"新浪微博";
+        _description = [NSString stringWithFormat:@"Total %d APIs,open.weibo.com",[_apis count]];
+        _appKey = @"261512368";
+        _appSecret = @"5884a2b28a9cca42c2df5dfd8b80f0e6";
+        _oauthRequestURL = [NSURL URLWithString:@"http://api.t.sina.com.cn/oauth/request_token"];
+        _oauthAccessURL = [NSURL URLWithString:@"http://api.t.sina.com.cn/oauth/access_token"];
+        _oauthAuthorizeURL = [NSURL URLWithString:@"http://api.t.sina.com.cn/oauth/authorize"];
+ */
+
 @end
 
 
